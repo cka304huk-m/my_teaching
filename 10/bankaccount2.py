6b563affa320e740d940c4a8752d7da3ae26a634
CK@304HuK <cka304huk@yahoo.com>
2021-03-14 16:12:14 +0300

    Монеты и банк

---
+++ 10/bankaccount2.py
@@ -0,0 +1,37 @@
+# Класс BankAccount имитирует банковский счёт.
+
+class BankAccount:
+
+    # Метод __init__ принимает аргумент
+    # с остатком на счёте.
+    # Он присваивается атрибуту __balance.
+
+    def __init__(self, bal):
+        self.__balance = bal
+
+    # Метод deposit вносит
+    # на счёт вклад.
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    # Метод withdraw снимает сумму
+    # со счёта.
+
+    def withdraw(self, amount):
+        if self.__balance >= amount:
+            self.__balance -= amount
+        else:
+            print('Ошибка: недостаточно средств')
+
+    # Метод get_balance возвращает
+    # остаток средств на счёте.
+
+    def get_balance(self):
+        return self.__balance
+
+    # Метод __str__ возвращает строковое
+    # значение, сообщающее о состоянии объекта.
+
+    def __str__(self):
+        return 'Остаток составляет ₽' + format(self.__balance, '.2f')
